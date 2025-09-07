2928374fb78688100e049d29fe43b3ba1fd5a389
lien0219 <lien0001@163.com>
2025-09-08 02:06:40 +0800

    feat:更改配置

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
-  basePath: process.env.NODE_ENV === "production" ? "" : "",
+  basePath:
+    process.env.NODE_ENV === "production" ? "/learning-management-system" : "",
 
   images: {
     unoptimized: true,
